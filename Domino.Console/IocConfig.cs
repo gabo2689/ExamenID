1ee80799ecf808bdc425f1ec9c5712c1c829abb9
gabo2689 <gabrielhh26@gmail.com>
2015-02-22 00:56:52 -0600

    Add autofac console solution ...

---
+++ Domino.Console/IocConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Core;
+using Domino.Logic;
+using Domino.Logic.Interfaces;
+
+namespace Domino.Console
+{
+    public class IocConfig
+    {
+        public static IContainer RegisterDependences()
+        {
+            var builder = new ContainerBuilder();
+
+            //build.RegisterType<Board>().As<Ibo>()
+            builder.RegisterType<Game>().As<IGame>();
+            builder.RegisterType<Player>().As<IPlayer>();
+            builder.RegisterType<Random>().As<IRandom>();
+            builder.RegisterType<Stock>().As<IStock>();
+
+            return builder.Build();
+
+        }
+
+        
+    }
+
+
+}
--- Domino.Console/Program.cs
+++ Domino.Console/Program.cs
@@ -4,13 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Domino.Logic;
+using Autofac;
+using Domino.Logic.Interfaces;
 
 namespace Domino.Console
 {
     class Program
     {
+        private static IContainer _container;
         static void Main(string[] args)
         {
+
+            _container = IocConfig.RegisterDependences();
+
+            var _game = _container.Resolve<IGame>();
+
+            
             
             var player1 = new Player("Dennis");
             var player2 = new Player("Edward");
